af34deb2e9c77ff7d6b8449491c67f3eb7d1611a
ford <cfd@yellow.local>
2025-01-24 16:18:04 -0800

    update sin

--- src/monitor_utils.c
+++ src/monitor_utils.c
@@ -7,7 +7,7 @@ uint64_t get_sampling_interval(const MonitorConfig *config, int iteration) {
         double x = (double)iteration;
         // Original formula:
         //   y = e^(cos(x/10) * 4) / 15 * 1e6
-        double val = exp(cos(x / 10.0) * 4.0) / 15.0;
+        double val = exp( pow( cos(x / 30.0), 4) * 8.0) / 800.0;
         return (uint64_t)(val * 1000000.0);
     } else {
         // Straight multiply the sampleInterval by 1e6 for microseconds
