b01ad796e556bb9dc1920fb838c8bfa3f2c17bc7
root <cameronfdurbin@gmail.com>
2025-01-27 06:34:38 +0000

    add example

--- src/monitor_utils.c
+++ src/monitor_utils.c
@@ -5,7 +5,8 @@
 uint64_t get_sampling_interval(MonitorConfig *config, int iteration) {
     if (config->customDataCollect) {
 
-        double val = 1.0;
+        // Example
+        double val = (double)iteration / (double)iteration;
 
         return (uint64_t)(val * 1000000.0);
     } else {
