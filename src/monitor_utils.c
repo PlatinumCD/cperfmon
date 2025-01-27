f946d93e27ed388ba9d2a37113ec175a097c9ccc
root <cameronfdurbin@gmail.com>
2025-01-27 06:24:55 +0000

    update custom

--- src/monitor_utils.c
+++ src/monitor_utils.c
@@ -20,7 +20,7 @@ uint64_t get_sampling_interval(MonitorConfig *config, int iteration) {
             val = 1.0;
         }
 
-        return (uint64_t)(val * 1000000.0);
+        return (uint64_t)(config->sampleInterval * 1000000.0);
     } else {
         // Straight multiply the sampleInterval by 1e6 for microseconds
         return (uint64_t)(config->sampleInterval * 1000000.0);
